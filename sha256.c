4640dc057fb9e202ad4215a0eea64b690330aa19
curley147 <michealcurley147@theory-of-algorithms.europe-west2-c.c.majestic-tape-229512.internal>
2019-03-27 00:55:44 +0000

    steps of algorithm roughly implemented

--- sha256.c
+++ sha256.c
@@ -53,22 +53,55 @@ void sha256(){
 	int t;
 
 	// from page 22. W[t] = M[t] for 0<=t<=15
-	for(t=0, t<16, t++)
+	for(t=0; t<16;	t++)
 		W[t]=M[t];
-	//from page 22. W[t] = ...
-	for(t=16, t<64, t++)
-		sig_1(W[t-2])+W[t-7]+sig_0(W[t-15])+W[t-16];
+	// from page 22. W[t] = ...
+	for(t=16; t<64; t++)
+		sig1(W[t-2])+W[t-7]+sig0(W[t-15])+W[t-16];
 
+	// Initialise a,b,c,d,e,f,g,h as per step 2, page 22
+	a=H[0]; b=H[1]; c=H[2]; d=H[3]; e=H[4]; f=H[5]; g=H[6]; h=H[7];
+	
+	// Step 3
+	for(t=0; t<64; t++){
+		T1 = h + SIG1(e) + Che(e,f,g) + K[t] + W[t];
+		T2 = SIG0(a) + Maj(a,b,c);
+		h=g;
+		g=f;
+		f=e;
+		e=d+T1;
+		d=c;
+		c=b;
+		b=a;
+		a=T1+T2;
+	}
 
+	// Step 4
+	H[0] = a+H[0];
+	H[1] = b+H[1];
+	H[2] = c+H[2];
+	H[3] = d+H[3];
+	H[4] = e+H[4];
+	H[5] = f+H[5];
+	H[6] = g+H[6];
+	H[7] = h+H[7];
+}
+
+// See section 3.2 for definitions
+uint32_t rotr(uint32_t n, uint32_t x){
+	return (x >> n) | (x << (32-n));
+}
+uint32_t shr(uint32_t n, uint32_t x){
+	return (x >> n);
 }
 
 uint32_t sig0(uint32_t x){
-	//See Sections 3.2 and 4.1.2 for definitions
-	//ROTR_n(x) = (x >> n)|(x << (32-n))
-	//SHR_n(x) = (x >> n)
-
+	// See Sections 3.2 and 4.1.2 for definitions
+	return rotr(7, x) ^ rotr(18, x) ^ shr(3, x));
 }
 uint32_t sig1(uint32_t x){
+	// See Sections 3.2 and 4.1.2 for definitions
+	return rotr(17, x) ^ rotr(19, x) ^ shr(10, x));
 
 }
 
